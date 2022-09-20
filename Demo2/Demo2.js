8499f85f3f4939522e01c72cf9e97feb882633bf
Mso Gvo <guoyuys@gmail.com>
2022-09-20 03:07:36 -0700

    delete

--- Demo2/Demo2.js
+++
@@ -1,8 +0,0 @@
-const canvas = document.getElementById('Demo2')
-const ctx = canvas.getContext('2d')
-
-function draw () {
-    ctx.fillstyle = 'rgb(250, 0, 0, 0.5)'
-    ctx.fillRect(300, 300, 50, 50);
-}
-
